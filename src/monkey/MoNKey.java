c1ce6ef2844d3a0ea36cb15cd0e10cf919624041
Stefano Volpe <stefano.volpe2@studio.unibo.it>
2021-05-02 01:15:34 +0200

    Prepare MoNKey for AI

--- src/monkey/MoNKey.java
+++ src/monkey/MoNKey.java
@@ -1,32 +1,25 @@
 package monkey;
 
 import java.util.Random;
-import mnkgame.*;
+import mnkgame.MNKCell;
+import mnkgame.MNKPlayer;
 
 /**
  * Totally random software player.
  */
 public class MoNKey implements MNKPlayer {
-	private Random rand;
-
-	/**
-	 * Default empty constructor
-	 */
-	public MoNKey() {
-	}
 
 	public void initPlayer(int M, int N, int K, boolean first, int timeout_in_secs) {
-		rand = new Random(System.currentTimeMillis());
+		rand = new Random(System.currentTimeMillis()); // TODO use AI instead
 	}
 
-	/**
-	 * Selects a random cell in <code>FC</code>
-	 */
 	public MNKCell selectCell(MNKCell[] FC, MNKCell[] MC) {
-		return FC[rand.nextInt(FC.length)];
+		return FC[rand.nextInt(FC.length)]; // TODO use AI instead
 	}
 
 	public String playerName() {
 		return "🅼🐵🅽🅺ey";
 	}
+
+	private Random rand;
 }
