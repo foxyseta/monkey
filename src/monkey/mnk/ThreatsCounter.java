bb4ab432b8fb5dce3b4feaf8a0e9a843314a2b53
Stefano Volpe <stefano.volpe2@studio.unibo.it>
2021-07-08 01:50:55 +0200

    Mockup ThreatsCounter implementation

--- src/monkey/mnk/ThreatsCounter.java
+++ src/monkey/mnk/ThreatsCounter.java
@@ -19,12 +19,12 @@ public class ThreatsCounter {
 	 * Constructs a new {@link ThreatsCounter}.
 	 *
 	 * @param threat {@link Threat} to keep track of.
-	 * @author Stefano Volpe
+	 * @author Gaia Clerici
 	 * @version 1.0
 	 * @since 1.0
 	 */
 	public ThreatsCounter(Threat threat) {
-		THREAT = threat;
+		THREAT = null; // TODO missing implementation
 	}
 
 	/**
@@ -33,15 +33,12 @@ public class ThreatsCounter {
 	 * null</code>.
 	 *
 	 * @param player Specifies whose counter is to be incremented.
-	 * @author Stefano Volpe
+	 * @author Gaia Clerici
 	 * @version 1.0
 	 * @since 1.0
 	 */
 	public void increment(Player player) {
-		if (player == Player.P1)
-			++p1counter;
-		else
-			++p2counter;
+		// TODO missing implementation
 	}
 
 	/**
@@ -51,20 +48,12 @@ public class ThreatsCounter {
 	 *
 	 * @param player Specifies whose counter is to be decremented.
 	 * @throws IllegalCallerException The counter is already at 0.
-	 * @author Stefano Volpe
+	 * @author Gaia Clerici
 	 * @version 1.0
 	 * @since 1.0
 	 */
 	public void decrement(Player player) {
-		if (player == Player.P1) {
-			if (p1counter == 0)
-				throw new IllegalCallerException("P1's counter is already at 0");
-			--p1counter;
-		} else {
-			if (p2counter == 0)
-				throw new IllegalCallerException("P2's counter is already at 0");
-			--p2counter;
-		}
+		// TODO missing implementation
 	}
 
 	/**
@@ -75,19 +64,17 @@ public class ThreatsCounter {
 	 *               <code>null</code>.
 	 * @throws IllegalArgumentException player is <code>null</code>.
 	 * @return The current value of the desired counter.
-	 * @author Stefano Volpe
+	 * @author Gaia Clerici
 	 * @version 1.0
 	 * @since 1.0
 	 */
 	public int get(Player player) {
-		if (player == null)
-			throw new IllegalArgumentException("player is null.");
-		return player == Player.P1 ? p1counter : p2counter;
+		return 0;// TODO missing implementation
 	}
 
 	/** Number of {@link Threat}s by the first {@link monkey.ai.Player Player}. */
-	private int p1counter = 0;
+	private int p1counter = -1; // TODO missing inizialization
 	/** Number of {@link Threat}s by the second {@link monkey.ai.Player Player}. */
-	private int p2counter = 0;
+	private int p2counter = -1; // TODO missing inizialization
 
 }
